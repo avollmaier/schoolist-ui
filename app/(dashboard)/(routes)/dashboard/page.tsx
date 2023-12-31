c1a5903421496892bf1a309091e16b0ee307487b
avollmaier <alois.vollm@gmail.com>
2023-12-31 12:30:18 +0100

    feat: restructuring of dashboard elements

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -2,7 +2,7 @@
 
 import {ArrowRight, CalendarDays} from "lucide-react";
 import Link from "next/link";
-import {Card} from "@/components/ui/card";
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import {cn} from "@/lib/utils";
 import {useRouter} from "next/navigation";
 import {Separator} from "@/components/ui/separator";
@@ -31,7 +31,7 @@ const DashboardPage = () => {
             href: "/grades",
         },
         {
-            label: "Classroom-Material",
+            label: "Material",
             icon: CalendarDays,
             color: "text-orange-700",
             bgColor: "bg-orange-700/10",
@@ -41,40 +41,37 @@ const DashboardPage = () => {
     const router = useRouter();
 
     return (
-        <div className={"flex flex-col"}>
-            <div className={"mb-8 space-y-4"}>
-                <h2 className={"text-2xl md:text-4xl font-bold text-center"}>
-                    Explore the power of Schoolist
+        <div className={"flex justify-between flex-col flex-grow"}>
+            <div className="mb-8 space-y-4">
+                <h2 className="text-2xl md:text-4xl font-bold text-center">
+                    Explore the power of AI
                 </h2>
-                <p className={"text-muted-foreground font-light text-sm md_text-lg text-center"}>
-                    The best teacher management system for your school.
+                <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+                    Chat with the smartest AI - Experience the power of AI
                 </p>
             </div>
-            <div className={"flex flex-wrap justify-around items-center"}>
+            <div className="px-4 md:px-8 lg:px-12 gap-4 grid grid-cols-2 mb-12">
                 {tools.map((tool) => (
-                    <Link href={tool.href} key={tool.href} className={"flex-1 px-4 md:px-26 lg:px-43"}>
-                        <Card
-                            onClick={() => router.push(tool.href)}
-                            key={tool.href}
-                            className={"p-4 border-black/5 flex items-center justify-center justify-between hover:shadow-md transition cursor-pointer"}>
-
-
-                            <div className={"flex items-center gap-x-4"}>
-                                <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                                    <tool.icon className={cn("w-8 h-8", tool.color)}/>
-                                </div>
-                                <div className={"font-semibold"}>
-                                    <h3 className={"text-lg font-bold"}>{tool.label}</h3>
-                                </div>
-                                <ArrowRight className={"w-5 h-5"}/>
+                    <Card onClick={() => router.push(tool.href)} key={tool.href}
+                          className="p-4 border-black/5 flex items-center hover:shadow-md transition cursor-pointer">
+                        <div className="flex items-center justify-between flex-grow">
+                            <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
+                                <tool.icon className={cn("w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7", tool.color)}/>
                             </div>
-
-                        </Card>
-
-                    </Link>
+                            <div className="font-semibold text-base md:text-lg lg:text-xl whitespace-nowrap p-2">
+                                {tool.label}
+                            </div>
+                            <div>
+                                <ArrowRight className="w-5 h-5 md:w-6 md:h-6 lg:w-7 lg:h-7"/>
+                            </div>
+                        </div>
+                    </Card>
                 ))}
             </div>
+            <div>
+               <Separator/>
+            </div>
         </div>
-    )
+    );
 }
 export default DashboardPage
